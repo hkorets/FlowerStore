23c5a8b0b507dbcadec7c88dd510c6e4e6882ad3
hkorets <hkorets0411@gmail.com>
2024-10-13 11:33:40 +0300

    Flower bucket updates

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,33 @@
 package flower.store;
 
-import lombok.Getter;
-import lombok.Setter;
-
-
-
-@Setter
 public class Flower {
-    @Getter
     private double sepalLength;
     private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
     private FlowerType flowerType;
 
     public String getColor() {
         return color.toString();
+    }
+    public void setColor(FlowerColor color) {
+        this.color = color;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public void setSepalLength(double len) {
+        this.sepalLength = len;
+    }
+    public double getSepalLength(){
+        return this.sepalLength;
+    }
+    public void setFlowerType(FlowerType type){
+        this.flowerType = type;
+    }
+    public FlowerType getFlowerType(){
+        return this.flowerType;
     }
 }
